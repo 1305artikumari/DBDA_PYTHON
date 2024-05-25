86da35415e2ed146f87c95a758ba07fecbd3e13f
Arti Kumari <107554479+1305artikumari@users.noreply.github.com>
2024-05-26 02:22:10 +0530

    Update 01-Polymorphism01.py

--- Python_Theory/Polymorphism/01-Polymorphism01.py
+++ Python_Theory/Polymorphism/01-Polymorphism01.py
@@ -1,18 +1,17 @@
 #  Polymorphism in Python
-#  What is Polymorphism?
-# The literal meaning of polymorphism is the condition of occurrence in different forms.
-# Polymorphism is a very important concept in programming. 
-# It refers to the use of a single type entity (method, operator or object) to represent different types in different scenarios.
+What is Polymorphism?
+The literal meaning of polymorphism is the condition of occurrence in different forms.
+Polymorphism is a very important concept in programming. 
+It refers to the use of a single type entity (method, operator or object) to represent different types in different scenarios.
 
 #  Method Overloading in Python
-
-# Method Overloading in Python is a type of Compile-time Polymorphism using which we can define two or 
-# more methods in the same class with the same name but with a different parameter list.
-# We cannot perform method overloading in the Python programming language as everything is 
-# considered an object in Python. Out of all the definitions with the same name, it uses the latest definition for the method. 
-# Hence, we can define numerous methods with the same name, but we can only use the latest defined method.
-# In Python, we can make our code have the same features as overloaded functions by defining a method in such
-# a way that there exists more than one way to call it. For example:
+ Method Overloading in Python is a type of Compile-time Polymorphism using which we can define two or 
+ more methods in the same class with the same name but with a different parameter list.
+ We cannot perform method overloading in the Python programming language as everything is 
+ considered an object in Python. Out of all the definitions with the same name, it uses the latest definition for the method. 
+ Hence, we can define numerous methods with the same name, but we can only use the latest defined method.
+ In Python, we can make our code have the same features as overloaded functions by defining a method in such
+ a way that there exists more than one way to call it. For example:
 
 # Function to take multiple arguments
 def sum_number(*args):
